fac66db1680eb750c427fa469520062f21435e13
Sagr <sagr3272@gmail.com>
2022-04-21 02:07:45 +0200

    Added Book to diffrent lists

--- app/src/main/java/com/sagr/my_library/activities/BookDetailsActivity.java
+++ app/src/main/java/com/sagr/my_library/activities/BookDetailsActivity.java
@@ -4,62 +4,163 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.sagr.my_library.R;
 import com.sagr.my_library.models.Book;
 import com.sagr.my_library.utils.Utils;
 
+import java.util.ArrayList;
+
 public class BookDetailsActivity extends AppCompatActivity {
 
 
     private ImageView bookImage;
-    private Button addToCurrentlyReading,addToFavourites,addToWantToRead,addToAlreadyRead;
-    private TextView bookName,bookAuthor,bookDescription,pagesCount;
+    private Button addToCurrentlyReading, addToFavourites, addToWantToRead, addToAlreadyRead;
+    private TextView bookName, bookAuthor, bookDescription, pagesCount;
     public static String BOOK_ID_KEY = "book_id";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_details);
         initViews();
         Intent intent = getIntent();
-        if(intent !=null){
-            int bookId = intent.getIntExtra(BOOK_ID_KEY,-1);
-            if(bookId != -1){
-                setData(Utils.getInstance().getBookById(bookId));
+
+        if (intent != null) {
+            int bookId = intent.getIntExtra(BOOK_ID_KEY, -1);
+            if (bookId != -1) {
+                Book incomingBook = Utils.getInstance().getBookById(bookId);
+                setData(incomingBook);
+                handleAlreadyRead(incomingBook);
+                handleCurrentlyRead(incomingBook);
+                handleFavurite(incomingBook);
+                handleWantToRead(incomingBook);
             }
 
         }
 
 
-
     }
 
-    private void  initViews(){
+    void handleAlreadyRead(Book book) {
+        ArrayList<Book> alreadyReadList = Utils.getAlreadyReadList();
+        boolean existInAlreadyRead = Utils.getInstance().existInList(book,alreadyReadList);
+        Log.i("EXIST","existInAlreadyRead"+existInAlreadyRead);
+
+        if(existInAlreadyRead){
+            addToAlreadyRead.setEnabled(false);
+        }
+        else{
+            addToAlreadyRead.setOnClickListener((View v)->{
+                if(Utils.getInstance().addToAlreadyReady(book)){
+                    Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(this,AlreadyReadActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(this,"error , please try again later",Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+    }
+
+    void handleCurrentlyRead(Book book) {
+        ArrayList<Book> currentlyReadList = Utils.getCurrentlyReadingList();
+        boolean existInCurrentlyRead = Utils.getInstance().existInList(book,currentlyReadList);
+        Log.i("EXIST","existInCurrentlyRead"+existInCurrentlyRead);
+        if(existInCurrentlyRead){
+            addToCurrentlyReading.setEnabled(false);
+        }
+        else{
+            addToCurrentlyReading.setOnClickListener((View v)->{
+                if(Utils.getInstance().addToCurrentlyRead(book)){
+                    Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(this,CurrentlyReadActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(this,"error , please try again later",Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+    }
+
+    void handleFavurite(Book book) {
+        ArrayList<Book> favouriteList = Utils.getFavouriteList();
+        boolean existInFavourite = Utils.getInstance().existInList(book,favouriteList);
+        Log.i("EXIST","existInFavourite"+existInFavourite);
+        if(existInFavourite){
+            addToFavourites.setEnabled(false);
+        }
+        else{
+            addToFavourites.setOnClickListener((View v)->{
+                if(Utils.getInstance().addToFavourite(book)){
+                    Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(this,FavouriteActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(this,"error , please try again later",Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+    }
+
+    void handleWantToRead(Book book) {
+        ArrayList<Book> wantToReadList = Utils.getWantToReadList();
+        boolean existInWantToRead = Utils.getInstance().existInList(book,wantToReadList);
+        Log.i("EXIST","existInWantToRead"+existInWantToRead);
+        if(existInWantToRead){
+            addToWantToRead.setEnabled(false);
+        }
+        else{
+            addToWantToRead.setOnClickListener((View v)->{
+                if(Utils.getInstance().addToWantToRead(book)){
+                    Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(this,WantToReadActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(this,"error , please try again later",Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+    }
+
+
+    private void initViews() {
         bookImage = (ImageView) findViewById(R.id.imgBookImage);
-        addToCurrentlyReading =(Button) findViewById(R.id.btnAddToCurrentlyReading);
-        addToFavourites =(Button) findViewById(R.id.btnAddToFavourites);
-        addToWantToRead =(Button) findViewById(R.id.btnAddToWantToRead);
-        addToAlreadyRead =(Button) findViewById(R.id.btnAddToAlreadyRead);
+        addToCurrentlyReading = (Button) findViewById(R.id.btnAddToCurrentlyReading);
+        addToFavourites = (Button) findViewById(R.id.btnAddToFavourites);
+        addToWantToRead = (Button) findViewById(R.id.btnAddToWantToRead);
+        addToAlreadyRead = (Button) findViewById(R.id.btnAddToAlreadyRead);
         bookName = (TextView) findViewById(R.id.txtBookName);
-        bookAuthor =(TextView) findViewById(R.id.txtAuthor);
-        bookDescription =(TextView) findViewById(R.id.txtDesc);
-        pagesCount = (TextView)findViewById(R.id.txtPagesCount);
+        bookAuthor = (TextView) findViewById(R.id.txtAuthor);
+        bookDescription = (TextView) findViewById(R.id.txtDesc);
+        pagesCount = (TextView) findViewById(R.id.txtPagesCount);
     }
 
-   private void setData(Book book){
+    private void setData(Book book) {
 
         bookName.setText(book.getBookName());
         bookAuthor.setText(book.getAuthorName());
         bookDescription.setText(book.getLongDesc());
         pagesCount.setText(String.valueOf(book.getPages()));
-       Glide.with(this)
-               .load(book.getImageUrl())
-               .error(R.drawable.ic_error)
-               .into(bookImage);
+        Glide.with(this)
+                .load(book.getImageUrl())
+                .error(R.drawable.ic_error)
+                .into(bookImage);
 
-   }
+    }
 }
--- app/src/main/java/com/sagr/my_library/utils/Utils.java
+++ app/src/main/java/com/sagr/my_library/utils/Utils.java
@@ -16,37 +16,36 @@ public class Utils {
     private static ArrayList<Book> wantToReadList;
 
 
-
     public Utils() {
 
-        if(allBooksList==null){
+        if (allBooksList == null) {
             allBooksList = new ArrayList<>();
             initData();
         }
-        if(currentlyReadingList == null){
+        if (currentlyReadingList == null) {
             currentlyReadingList = new ArrayList<>();
 
         }
-        if(alreadyReadList == null){
+        if (alreadyReadList == null) {
             alreadyReadList = new ArrayList<>();
         }
-        if(favouriteList == null){
-            favouriteList= new ArrayList<>();
+        if (favouriteList == null) {
+            favouriteList = new ArrayList<>();
         }
-        if(wantToReadList == null ){
+        if (wantToReadList == null) {
             wantToReadList = new ArrayList<>();
         }
     }
 
 
-    void initData(){
-        allBooksList.add(new Book(1,"Eloquent JavaScript, Third Edition","https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg","A Modern Introduction to Programming","JavaScript lies at the heart of almost every modern web application, from social apps like Twitter to browser-based game frameworks like Phaser and Babylon. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications.","Marijn Haverbeke",472));
-        allBooksList.add(       new Book(2,"Practical Modern JavaScript","https://images-na.ssl-images-amazon.com/images/I/51LHTs-4ziL._SX331_BO1,204,203,200_.jpg","Dive into ES6 and the Future of JavaScript","To get the most out of modern JavaScript, you need learn the latest features of its parent specification, ECMAScript 6 (ES6). This book provides a highly practical look at ES6, without getting lost in the specification or its implementation details.",
-                "Nicolás Bevacqua",334));
-        allBooksList.add(new Book(3,"Understanding ECMAScript 6","https://images-na.ssl-images-amazon.com/images/I/817f4ZWXjhL.jpg","The Definitive Guide for JavaScript Developers","ECMAScript 6 represents the biggest update to the core of JavaScript in the history of the language. In Understanding ECMAScript 6, expert developer Nicholas C. Zakas provides a complete guide to the object types, syntax, and other exciting changes that ECMAScript 6 brings to JavaScript.",
-                "Nicholas C. Zakas",352   ));
-        allBooksList.add(new Book(4,"Speaking JavaScript","https://images-na.ssl-images-amazon.com/images/I/91sXL7wrjgL.jpg","An In-Depth Guide for Programmers","Like it or not, JavaScript is everywhere these days -from browser to server to mobile- and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who once found himself in the same position.",
-                "O'Reilly Media",460));
+    void initData() {
+        allBooksList.add(new Book(1, "Eloquent JavaScript, Third Edition", "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg", "A Modern Introduction to Programming", "JavaScript lies at the heart of almost every modern web application, from social apps like Twitter to browser-based game frameworks like Phaser and Babylon. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications.", "Marijn Haverbeke", 472));
+        allBooksList.add(new Book(2, "Practical Modern JavaScript", "https://images-na.ssl-images-amazon.com/images/I/51LHTs-4ziL._SX331_BO1,204,203,200_.jpg", "Dive into ES6 and the Future of JavaScript", "To get the most out of modern JavaScript, you need learn the latest features of its parent specification, ECMAScript 6 (ES6). This book provides a highly practical look at ES6, without getting lost in the specification or its implementation details.",
+                "Nicolás Bevacqua", 334));
+        allBooksList.add(new Book(3, "Understanding ECMAScript 6", "https://images-na.ssl-images-amazon.com/images/I/817f4ZWXjhL.jpg", "The Definitive Guide for JavaScript Developers", "ECMAScript 6 represents the biggest update to the core of JavaScript in the history of the language. In Understanding ECMAScript 6, expert developer Nicholas C. Zakas provides a complete guide to the object types, syntax, and other exciting changes that ECMAScript 6 brings to JavaScript.",
+                "Nicholas C. Zakas", 352));
+        allBooksList.add(new Book(4, "Speaking JavaScript", "https://images-na.ssl-images-amazon.com/images/I/91sXL7wrjgL.jpg", "An In-Depth Guide for Programmers", "Like it or not, JavaScript is everywhere these days -from browser to server to mobile- and now you, too, need to learn the language or dive deeper than you have. This concise book guides you into and through JavaScript, written by a veteran programmer who once found himself in the same position.",
+                "O'Reilly Media", 460));
     }
 
     public static ArrayList<Book> getAllBooksList() {
@@ -71,19 +70,45 @@ public class Utils {
 
 
 
+    public boolean addToCurrentlyRead(Book book) {
+        return currentlyReadingList.add(book);
+    }
+
+    public boolean addToFavourite(Book book) {
+        return favouriteList.add(book);
+    }
+
+    public boolean addToWantToRead(Book book) {
+        return wantToReadList.add(book);
+    }
+
+    public boolean addToAlreadyReady(Book book) {
+        return alreadyReadList.add(book);
+    }
+
+
     public static Utils getInstance() {
-        if(instance ==null){
+        if (instance == null) {
             instance = new Utils();
         }
         return instance;
     }
 
-    public  Book getBookById(int bookId){
-        for(Book b : allBooksList){
-            if(b.getId() == bookId){
-                return  b;
+    public Book getBookById(int bookId) {
+        for (Book b : allBooksList) {
+            if (b.getId() == bookId) {
+                return b;
             }
         }
         return null;
+    }
+
+    public boolean existInList(Book book, ArrayList<Book> list) {
+        for (Book b : list) {
+            if (b == book) {
+                return true;
+            }
+        }
+        return false;
     }
 }
